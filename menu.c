e1def9ff5238494da31414d5d47102721ff9d1ef
elxsson <soullcakezz@gmail.com>
2025-08-16 01:24:48 -0300

    feat: adiciona escolha no menu

--- menu.c
+++ menu.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
+    int opcao;
+
     printf("\n╔══════════════════════════════════════════════════╗\n");
     printf("║ SIG-MASSAGE - SISTEMA DE GESTÃO DE MASSAGENS     ║\n");
     printf("╠══════════════════════════════════════════════════╣\n");
@@ -13,6 +15,32 @@ int main() {
     printf("║ ► 0. Sair do Sistema                             ║\n");
     printf("║                                                  ║\n");
     printf("╚══════════════════════════════════════════════════╝\n");
+
+    printf("\n Digite a opção desejada: ");
+    scanf("%d", &opcao);
+
+    switch(opcao) {
+        case 1:
+            printf("\nVocê escolheu o módulo CLIENTES.\n");
+            break;
+        case 2:
+            printf("\nVocê escolheu o módulo MASSOTERAPEUTAS.\n");
+            break;
+        case 3:
+            printf("\nVocê escolheu o módulo SERVIÇOS.\n");
+            break;
+        case 4:
+            printf("\nVocê escolheu o módulo AGENDAMENTOS.\n");
+            break;
+        case 5:
+            printf("\nVocê escolheu o módulo RELATÓRIOS.\n");
+            break;
+        case 0:
+            printf("\nSaindo do sistema...\n");
+            break;
+        default:
+            printf("\nOpção inválida!\n");
+    }
 
     return 0;
 }
