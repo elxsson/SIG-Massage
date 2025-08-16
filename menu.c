4bdc598ee1be0a5ead57c3d9e149e26b30b323eb
elxsson <soullcakezz@gmail.com>
2025-08-16 01:12:56 -0300

    feat: adiciona modelo de interface
    provavelmente as proximas interfaces do projeto seguirao esse padrao

---
+++ menu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("\n╔══════════════════════════════════════════════════╗\n");
+    printf("║ SIG-MASSAGE - SISTEMA DE GESTÃO DE MASSAGENS     ║\n");
+    printf("╠══════════════════════════════════════════════════╣\n");
+    printf("║                                                  ║\n");
+    printf("║ ► 1. Módulo Clientes                             ║\n");
+    printf("║ ► 2. Módulo Massoterapeutas                      ║\n");
+    printf("║ ► 3. Módulo Serviços                             ║\n");
+    printf("║ ► 4. Módulo Agendamentos                         ║\n");
+    printf("║ ► 5. Módulo Relatórios                           ║\n");
+    printf("║ ► 0. Sair do Sistema                             ║\n");
+    printf("║                                                  ║\n");
+    printf("╚══════════════════════════════════════════════════╝\n");
+
+    return 0;
+}
