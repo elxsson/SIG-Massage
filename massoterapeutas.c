8936c0c3941f8dd7bd0e3f8f3e00d84716f8b0cf
elxsson <soullcakezz@gmail.com>
2025-08-23 19:35:46 -0300

    feat: adiciona arquivo de massoterapeutas

---
+++ massoterapeutas.c
@@ -0,0 +1,32 @@
+/*
+ |===========================================================================|
+ |                         MÓDULO MASSOTERAPEUTAS                           |
+ |        CRUD para Gestão de Massoterapeutas do SIG-MASSAGE                |
+ |===========================================================================|
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void pausar() {
+    printf("\n Pressione Enter para continuar...");
+    while(getchar() != '\n');
+    getchar();
+}
+
+void mostrarMenuMassoterapeutas() {
+    system("clear || cls");
+    printf("\n╔══════════════════════════════════════════════╗\n");
+    printf("║             MODULO MASSOTERAPEUTA           ║\n");
+    printf("╠══════════════════════════════════════════════╣\n");
+    printf("║                                              ║\n");
+    printf("║ 1. Cadastrar Massoterapeuta                  ║\n");
+    printf("║ 2. Listar Massoterapeutas                    ║\n");
+    printf("║ 3. Procurar Massoterapeuta                   ║\n");
+    printf("║ 4. Atualizar Dados do Massoterapeuta         ║\n");
+    printf("║ 5. Excluir Massoterapeuta                    ║\n");
+    printf("║ 0. Voltar ao Menu Principal                  ║\n");
+    printf("║                                              ║\n");
+    printf("╚══════════════════════════════════════════════╝\n");
+    printf("\n Digite a opção desejada: ");
+}
