08e3fef16741199703f4acceba1456af17c7fe3c
elxsson <soullcakezz@gmail.com>
2025-09-01 21:05:30 -0300

    feat: adiciona tela inicial do modulo

---
+++ produtos.c
@@ -0,0 +1,26 @@
+/*
+|=============================================================|
+|                      MODULO PRODUTOS                        |
+| CRUD para a gestão de produtos do sistema SIG-MASSAGE        |
+|=============================================================|
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void menuProdutos() {
+    limparTela();
+    printf("\n╔══════════════════════════════════════════════╗\n");
+    printf("║                MODULO PRODUTOS               ║\n");
+    printf("╠══════════════════════════════════════════════╣\n");
+    printf("║                                              ║\n");
+    printf("║ 1. Cadastrar Produto                         ║\n");
+    printf("║ 2. Listar Produtos                           ║\n");
+    printf("║ 3. Procurar Produto                          ║\n");
+    printf("║ 4. Atualizar Dados do Produto                ║\n");
+    printf("║ 5. Excluir Produto                           ║\n");
+    printf("║ 0. Voltar ao Menu Principal                  ║\n");
+    printf("║                                              ║\n");
+    printf("╚══════════════════════════════════════════════╝\n");
+    printf("\n Digite a opção desejada: ");
+}
