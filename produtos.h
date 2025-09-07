64cce79562515ca65968afaff74e290950822cff
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-09-07 17:54:57 -0300

    feat: habilitar funções comentadas para funcionamento do sistema

--- produtos.h
+++ produtos.h
@@ -2,11 +2,17 @@
 #define PRODUTOS_H
 
 void menuProdutos();
-// void cadastroProduto();
-// void listarProdutos();
-// void buscarProduto();
-// void atualizarProduto();
-// void deletarProduto();
-// void produtos();
+
+void cadastroProduto();
+
+ void listarProdutos();
+
+ void buscarProduto();
+
+ void atualizarProduto();
+
+ void deletarProduto();
+ 
+ void produtos();
 
 #endif
