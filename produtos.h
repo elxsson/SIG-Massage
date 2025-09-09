941910184359dc30453f6a3f2e9e355c8a7778b2
elxsson <soullcakezz@gmail.com>
2025-09-08 23:51:17 -0300

    fix: atualiza header de produtos

--- produtos.h
+++ produtos.h
@@ -2,11 +2,11 @@
 #define PRODUTOS_H
 
 void menuProdutos();
-// void cadastroProduto();
-// void listarProdutos();
-// void buscarProduto();
-// void atualizarProduto();
-// void deletarProduto();
-// void produtos();
+void cadastroProduto();
+void listarProdutos();
+void buscarProduto();
+void atualizarProduto();
+void deletarProduto();
+void produtos();
 
 #endif
