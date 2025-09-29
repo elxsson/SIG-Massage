9b40025a3168115c5105e67c905158d58f9f83b2
Helena Dantas <helena.mariano.123@ufrn.edu.br>
2025-09-29 23:42:18 +0000

    feat: adiciona struct Produtos no header

--- include/produtos.h
+++ include/produtos.h
@@ -1,18 +1,19 @@
 #ifndef PRODUTOS_H
 #define PRODUTOS_H
 
+typedef struct {
+    char nome[70];
+    char codigo[20];
+    float preco;
+    int estoque;
+} Produtos;
+
 void produtos();
-
 void menuProdutos();
-
 void cadastroProduto();
-
 void listarProdutos();
-
 void buscarProduto();
-
 void atualizarProduto();
-
 void deletarProduto();
 
 #endif
