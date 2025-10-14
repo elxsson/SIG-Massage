82bc055da601612fa4f9eec3689245ab8f3f1197
elxsson <soullcakezz@gmail.com>
2025-10-13 21:06:41 -0300

    refactor: adiciona assinaturas das funcoes no header

--- include/utils.h
+++ include/utils.h
@@ -2,9 +2,12 @@
 #define UTILS_H
 
 void pausar();
-
 void limparBuffer();
-
 void limparTela();
+
+int lerNome(char *nome, int tamanho);
+int lerCPF(char *cpf, int tamanho);
+int lerTelefone(char *telefone, int tamanho);
+int lerEmail(char *email, int tamanho);
 
 #endif
