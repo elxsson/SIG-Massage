978bc84df1b9e8e6dfc91ae8664f41904c3107df
elxsson <soullcakezz@gmail.com>
2025-10-13 21:09:47 -0300

    refactor: atualiza struct de Cliente e refatora arquivo

--- include/clientes.h
+++ include/clientes.h
@@ -1,11 +1,12 @@
-#ifndef CLIENTE_H
-#define CLIENTE_H
+#ifndef CLIENTES_H
+#define CLIENTES_H
 
 typedef struct {
     char nome[70];
     char cpf[20];
     char telefone[20];
     char email[70];
+    int status; // 1 = ativo, 0 = inativo
 } Cliente;
 
 void clientes();
@@ -15,5 +16,10 @@ void listarClientes();
 void buscarCliente();
 void atualizarCliente();
 void deletarCliente();
+
+// funcoes auxiliares
+int salvarCliente(Cliente *c);
+int carregarClientes(Cliente **clientes, int *quantidade);
+int atualizarArquivoClientes(Cliente *clientes, int quantidade);
 
 #endif
