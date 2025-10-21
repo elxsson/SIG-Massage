8d15dc3284c6bda72c966ef6c6557dccc5de3735
elxsson <soullcakezz@gmail.com>
2025-10-20 23:52:54 -0300

    refactor: atualiza o header do modulo financeiro

--- include/financeiro.h
+++ include/financeiro.h
@@ -1,14 +1,22 @@
 #ifndef FINANCEIRO_H
 #define FINANCEIRO_H
 
+typedef struct {
+    char tipo;          // 'E' = entrada, 'S' = saida
+    float valor;
+    char descricao[100];
+    char cpf[20];       // CPF do massoterapeuta (saida)
+    int status;
+} Movimentacao;
+
 void financeiro();
-
 void menuFinanceiro();
-
 void registrarEntrada();
-
 void registrarSaida();
-
 void listarFinanceiro();
+
+int salvarMovimentacao(Movimentacao *m);
+int carregarMovimentacoes(Movimentacao **movimentacoes, int *quantidade);
+int atualizarArquivoMovimentacoes(Movimentacao *movimentacoes, int quantidade);
 
 #endif
